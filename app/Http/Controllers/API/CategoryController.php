54b76174692b51be04558e44a8117ca404f54deb
ZayYarNaing <zayarnaing.pp@gmail.com>
2026-03-05 16:06:50 +0630

    Add Category API CRUD

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -6,20 +6,24 @@ use App\Http\Controllers\API\BaseController;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
+use App\Repositories\Category\CategoryRepositoryInterface;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends BaseController
 {
+    protected $categoryRepository;
+    public function __construct(CategoryRepositoryInterface $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
     public function index()
     {
         // dd('here');
 
-        $categories = Category::get();
+        $categories = $this->categoryRepository->index();
 
         $data = CategoryResource::collection($categories);
-
-        // dd($categories);
 
         return $this->success($data, "Category Retrieved Successfully", 200);
     }
@@ -31,8 +35,6 @@ class CategoryController extends BaseController
             'description' => 'required|string',
             'image' => 'required'
         ]);
-
-        // dd($validator);
 
         if ($validator->fails()) {
             return $this->error("Validation Error", $validator->errors(), 422);
@@ -52,7 +54,41 @@ class CategoryController extends BaseController
         ]);
 
         return $this->success($category, "Category Created Successfully", 201);
+    }
 
+    public function show($id)
+    {
+        $category = Category::find($id);
 
+        $data = new CategoryResource($category);
+
+        return $this->success($data, "Category Show Scuccessfully", 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|string',
+            'description' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error('Validation Erro', $validator->errors(), 422);
+        }
+
+        $category = Category::find($id);
+
+        $category->update($request->all());
+
+        return $this->success($category, "Category Updated Successfully", 200);
+    }
+
+    public function delete($id)
+    {
+        $category = Category::find($id);
+
+        $category->delete();
+
+        return $this->success($category, "Category Deleted Successfully", 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,6 @@ Route::post('/auth/login', [AuthController::class, 'login']);
 
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::post('/categories', [CategoryController::class, 'store']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
+Route::put('/categories/{id}', [CategoryController::class, 'update']);
+Route::delete('/categories/{id}', [CategoryController::class, 'delete']);
